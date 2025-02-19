1f14c63fd13728dc56da21d6a0d1389668f73f0f
shanghua <shawnshawn423423@gmail.com>
2025-02-19 20:48:14 +0800

    第一節課

---
+++ gcc/plus.s
@@ -0,0 +1,40 @@
+	.file	"plus.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "The sum of %d and %d is %d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$1, -4(%rbp)
+	movl	$2, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %ecx
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%ecx, %r9d
+	movl	%edx, %r8d
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
